5e0686e74f04dd4edb259c81065e9e80aa0649f4
unknown <thalesvrpb21@gmail.com>
2023-05-29 14:08:46 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,12 @@ import express, { Application, json } from "express";
 import { loginRoutes } from "./routes/login";
 import { usersRoutes } from "./routes/users";
 import { contacRoutes } from "./routes/contac";
+import cors from "cors";
 
 const app: Application = express()
 app.use(express.json())
+
+app.use(cors())
 
 app.use("/users", usersRoutes)
 
